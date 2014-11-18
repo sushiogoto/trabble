61e61f299fab49299e8f82bd07f52c2cff114908
sushiogoto <yoshio.goto@gmail.com>
2014-11-18 16:52:28 +0800

    Add confirmable to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :trip_users
   has_many :trips, :through => :trip_users
