b414cc882fdb82b3b881ef077ff7b3a6f1f1775c
sushiogoto <yoshio.goto@gmail.com>
2014-11-19 17:28:40 +0800

    Complete devise invitable

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
-  devise_for :users
+  devise_for :users, controllers: { invitations: 'devise/invitations' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
