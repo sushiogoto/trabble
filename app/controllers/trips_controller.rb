5a079621510996867b9e2b5e727fa037a09fafae
sushiogoto <yoshio.goto@gmail.com>
2014-11-24 11:45:28 +0800

    Add location model and insert location into trip page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -19,6 +19,7 @@ class TripsController < ApplicationController
   # GET /trips/1
   # GET /trips/1.json
   def show
+    @locations = @trip.locations
   end
 
   # GET /trips/new
@@ -99,6 +100,6 @@ class TripsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def trip_params
-      params.require(:trip).permit(:location, :start_date, :end_date)
+      params.require(:trip).permit(:name, :start_date, :end_date)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121041831) do
+ActiveRecord::Schema.define(version: 20141123125457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.integer  "trip_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "locations", ["trip_id"], name: "index_locations_on_trip_id", using: :btree
 
   create_table "trip_users", force: true do |t|
     t.integer  "user_id"
