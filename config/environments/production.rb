a650f3c9edc374b87d553db7c7292ecd65bad3a0
sushiogoto <yoshio.goto@gmail.com>
2014-11-28 18:06:34 +0800

    Cleanup code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.perform_deliveries = false
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.delivery_method = :test
 end
